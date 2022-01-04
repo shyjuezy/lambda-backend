6e7e3076547adf835a280328350254f82aa26f6f
SHYJU VISWAMBARAN <shyjuezy@gmail.com>
2022-01-04 11:14:47 -0600

    S3 Read file

--- lib/lambda-backend-stack.ts
+++ lib/lambda-backend-stack.ts
@@ -1,39 +1,43 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { aws_apigateway as apigateway } from 'aws-cdk-lib';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as path from 'path';
 import { GenericTable } from './GenericTable';
-import { aws_lambda_nodejs as lambda_nodejs } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { S3 } from 'aws-sdk';
+// import { aws_lambda as lambda } from 'aws-cdk-lib';
+// import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+// import { aws_lambda_nodejs as lambda_nodejs } from 'aws-cdk-lib';
 
 export class LambdaBackendStack extends Stack {
   private api = new apigateway.RestApi(this, 'SampleApi');
-  private spacesTable = new GenericTable('SampleTable', 'sampleID', this);
+  // TODO private sampleTable = new GenericTable('SampleTable', 'sampleID', this);
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaFn = new lambda.Function(this, 'helloLambda', {
-      functionName: 'orig-lambda-hello',
-      runtime: Runtime.NODEJS_14_X,
-      handler: 'hello.main',
-      code: Code.fromAsset(path.join(__dirname, '../services/hello')),
+    // const lambdaFn = new lambda.Function(this, 'helloLambda', {
+    //   functionName: 'orig-lambda-hello',
+    //   runtime: Runtime.NODEJS_14_X,
+    //   handler: 'hello.main',
+    //   code: Code.fromAsset(path.join(__dirname, '../services/hello')),
+    // });
+
+    const lambdaNodjsFn = new NodejsFunction(this, 'helloLambdaNodejs', {
+      entry: path.join(__dirname, '../services/node-lambda/hello.ts'),
+      handler: 'handler',
+      functionName: 'node-lambda-hello',
     });
 
-    const lambdaNodjsFn = new lambda_nodejs.NodejsFunction(
-      this,
-      'helloLambdaNodejs',
-      {
-        entry: path.join(__dirname, '../services/node-lambda/hello.ts'),
-        handler: 'handler',
-        functionName: 'node-lambda-hello',
-      }
-    );
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+    lambdaNodjsFn.addToRolePolicy(s3ListPolicy);
 
     //Hello API Lambda Integration
-    const helloLambdaIntegration = new LambdaIntegration(lambdaFn);
+    const helloLambdaIntegration = new LambdaIntegration(lambdaNodjsFn);
     const helloLambdaResource = this.api.root.addResource('test'); // ("hello");
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,42 @@
-import { v4 } from 'uuid';
+import { aws_sdb } from 'aws-cdk-lib';
+// import { v4 } from 'uuid';
+import { S3 } from 'aws-sdk';
+import * as readline from 'readline';
+
+const s3Client = new S3();
 
 async function handler(event: any, context: any) {
+  const buckets = await s3Client.listBuckets().promise();
+
+  const params = {
+    Bucket: 'shyju-firstbucket',
+    Key: 'testfile.json',
+  };
+
+  let data = async function () {
+    let records = [{}];
+    const stream = s3Client.getObject(params).createReadStream();
+    let lineReader = readline.createInterface({ input: stream });
+
+    lineReader
+      .on('line', (line) => {
+        records.push(line);
+      })
+      .on('close', () => {
+        console.log('Finished processing S3 file.');
+        console.log(records);
+        return JSON.stringify(records);
+      });
+  };
+
+  const result = await data();
+
+  console.log('Got An Event:');
+  console.log(event);
+
   return {
     statusCode: 200,
-    body: 'Hello From Lambda!' + v4(),
+    body: 'Hello From Lambda!' + result,
   };
 }
 
